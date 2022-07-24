fb2daa6d0ee571b4f5f5930a185e14c41d702324
«evgeny» <solov-ghenya195@mail.ru>
2022-07-24 23:02:52 +0300

    add Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.Write(max);
