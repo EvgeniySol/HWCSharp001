8a217ca7bdbf21ca0962e776bfaeca20be3cc5f9
«evgeny» <solov-ghenya195@mail.ru>
2022-07-24 22:52:55 +0300

    add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите числа");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int max = x;
+int min = x;
+if(x>y)
+{
+    max = x;
+    min = y;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.Write(min);
+}
+else
+{
+    max = y;
+    min = x;
+    Console.Write("max = ");
+    Console.WriteLine(max);
+    Console.Write("min = ");
+    Console.Write(min);
+}
