0e731a48e72a89ae5abd3b7113ccffcc0a20026a
«evgeny» <solov-ghenya195@mail.ru>
2022-07-25 23:22:12 +0300

    add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Является ли число четным?");
+if (a%2==0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
